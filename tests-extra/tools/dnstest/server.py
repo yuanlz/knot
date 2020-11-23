a89c2c6d0b66fcb4958dd8278d78ee91e1f88fb5
Jan Hák <jan.hak@nic.cz>
2020-11-23 17:18:04 +0100

    tests-extra: Fix 'edns/nsid' when dnspython>=2.0
    Signed-off-by: Jan Hák <jan.hak@nic.cz>

--- tests-extra/tools/dnstest/server.py
+++ tests-extra/tools/dnstest/server.py
@@ -514,13 +514,6 @@ class Server(object):
 
         # Set EDNS.
         if edns != None or bufsize or nsid:
-            class NsidFix(object):
-                '''Current pythondns doesn't implement NSID option.'''
-                def __init__(self):
-                    self.otype = dns.edns.NSID
-                def to_wire(self, file=None):
-                    pass
-
             if edns:
                 edns = int(edns)
             else:
@@ -534,7 +527,7 @@ class Server(object):
             dig_flags += " +bufsize=%i" % payload
 
             if nsid:
-                options = [NsidFix()]
+                options = [dns.edns.GenericOption(dns.edns.NSID, b'')]
                 dig_flags += " +nsid"
             else:
                 options = None
